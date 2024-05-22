9f86c2127333632e7c1ca7a2e822cfdf392eb0df
Choi minyoung <84059199+mmin0-0@users.noreply.github.com>
2024-05-22 17:07:14 +0900

    ,

--- shop_page/src/App.js
+++ shop_page/src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 // import './App.css';
 import './assets/scss/common.scss';
 import Header from './components/Header.js';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/basket" element={<div>장바구니</div>} />
+      </Routes>
       <div className="title">안녕하세요</div>
     </div>
   );
