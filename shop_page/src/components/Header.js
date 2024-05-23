d31a14ce724629052f5859f6ed594f6753bef85d
Choi minyoung <84059199+mmin0-0@users.noreply.github.com>
2024-05-23 15:25:36 +0900

    ,

--- shop_page/src/components/Header.js
+++ shop_page/src/components/Header.js
@@ -1,8 +1,19 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import '../assets/scss/components/header.scss'
 
 function Header(){
   return (
-    <div className="header">
-      <div>header입니다.</div>
+    <div className="header-wrap">
+      <header>
+        <div className="inner">
+          <div className="logo">
+            <a href="#none">
+              <img src={`${process.env.PUBLIC_URL}/images/common/logo.svg`}   alt="감탄상회" />
+            </a>
+          </div>
+          <div className="menu"></div>
+        </div>
+      </header>
     </div>
   );
 }
