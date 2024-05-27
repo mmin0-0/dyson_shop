1180d096ae91d2a6030d9011d2751c4ec0c4ada2
Choi minyoung <84059199+mmin0-0@users.noreply.github.com>
2024-05-27 16:46:34 +0900

    ,

--- shop_page/src/components/Header.js
+++ shop_page/src/components/Header.js
@@ -20,36 +20,58 @@ function Header(){
           </div>
         </div>
         <div className="search-wrap">
-          <div className="search-inp">
-            <form action="submit">
-              <input type="search" placeholder="검색어를 입력해 주세요" />
-              <button type="submit" className="btn-search">검색</button>
-            </form>
-          </div>
-          <div className="search-keyword">
-            <strong>추천검색어</strong>
-            <ul>
-              <li><a href="#none">lorem</a></li>
-              <li><a href="#none">lorem</a></li>
-              <li><a href="#none">lorem</a></li>
-              <li><a href="#none">lorem</a></li>
-              <li><a href="#none">lorem</a></li>
-            </ul>
-          </div>
-          <div className="btn-closed">
-            <button type="button">CLOSED</button>
+          <div className="search-inner">
+            <div className="search-inp">
+              <form action="submit">
+                <div className="input-wrap">
+                  <label for="searchWrap" className="hide">검색</label>
+                  <input type="search" id="searchWrap" placeholder="검색어를 입력해 주세요" />
+                  <button type="submit" className="btn-search">검색</button>
+                </div>
+              </form>
+            </div>
+            <div className="search-keyword">
+              <strong>추천검색어</strong>
+              <ul>
+                <li><a href="#none">lorem</a></li>
+                <li><a href="#none">lorem</a></li>
+                <li><a href="#none">lorem</a></li>
+                <li><a href="#none">lorem</a></li>
+                <li><a href="#none">lorem</a></li>
+              </ul>
+            </div>
+            <div className="btn-closed">
+              <button type="button">CLOSED</button>
+            </div>
           </div>
         </div>
-          <div className="menu">
-            <ul className="menu-list">
-              <li><a href="#none">감탄상회 소개</a></li>
-              <li><a href="#none">감탄스토어</a></li>
-              <li><a href="#none">비건 케이터링</a></li>
-              <li><a href="#none">감탄클래스</a></li>
-              <li><a href="#none">캠페인</a></li>
-              <li><a href="#none">커뮤니티</a></li>
-            </ul>
-          </div>
+        <nav className="gnb-wrap">
+          <ul className="gnb-inner">
+            <li>
+              <a href="#none" className="depth1">감탄상회 소개</a>
+              <div className="gnb-draw">
+                <div className="draw-inner">
+                  <div className="menu-list">
+                    <ul>
+                      <li><a href="#none">브랜드</a></li>
+                      <li><a href="#none">감탄 스토리</a></li>
+                      <li><a href="#none">비전</a></li>
+                      <li><a href="#none">활동가 소개</a></li>
+                    </ul>
+                  </div>
+                  <div className="banner">
+                    배너
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li><a href="#none" className="depth1">감탄스토어</a></li>
+            <li><a href="#none" className="depth1">비건 케이터링</a></li>
+            <li><a href="#none" className="depth1">감탄클래스</a></li>
+            <li><a href="#none" className="depth1">캠페인</a></li>
+            <li><a href="#none" className="depth1">커뮤니티</a></li>
+          </ul>
+        </nav>
       </header>
     </>
   );
