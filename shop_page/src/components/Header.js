d93ed738b4b24ffbd3984c3cf596392e69fa76a8
Choi minyoung <84059199+mmin0-0@users.noreply.github.com>
2024-05-27 15:20:23 +0900

    ,

--- shop_page/src/components/Header.js
+++ shop_page/src/components/Header.js
@@ -3,24 +3,43 @@ import '../assets/scss/components/header.scss'
 
 function Header(){
   return (
-    <div className="header-wrap">
+    <>
       <header>
-        <div className="con-box">
-          <div className="hd-top">
-            <div className="logo">
+        <div className="hd-inner-wrap">
+          <div className="logo">
               <a href="#none">
                 <img src={`${process.env.PUBLIC_URL}/images/common/logo.svg`}   alt="감탄상회" />
               </a>
-            </div>
-            <div className="side-menu">
-              <div className="search-wrap">
-                <input type="search" placeholder="검색어를 입력해주세요." />
-              </div>
-              <div className="user">
-              <i class="fa-sharp fa-solid fa-basket-shopping-simple"></i>
-              </div>
-            </div>
           </div>
+          <div className="utility-wrap">
+              <div className="user">
+                유저
+                <i class="fa-sharp fa-solid fa-basket-shopping-simple"></i>
+              </div>
+              <div className="search-controls">검색</div>
+          </div>
+        </div>
+        <div className="search-wrap">
+          <div className="search-inp">
+            <form action="submit">
+              <input type="search" placeholder="검색어를 입력해 주세요" />
+              <button type="submit" className="btn-search">검색</button>
+            </form>
+          </div>
+          <div className="search-keyword">
+            <strong>추천검색어</strong>
+            <ul>
+              <li><a href="#none">lorem</a></li>
+              <li><a href="#none">lorem</a></li>
+              <li><a href="#none">lorem</a></li>
+              <li><a href="#none">lorem</a></li>
+              <li><a href="#none">lorem</a></li>
+            </ul>
+          </div>
+          <div className="btn-closed">
+            <button type="button">CLOSED</button>
+          </div>
+        </div>
           <div className="menu">
             <ul className="menu-list">
               <li><a href="#none">감탄상회 소개</a></li>
@@ -31,9 +50,8 @@ function Header(){
               <li><a href="#none">커뮤니티</a></li>
             </ul>
           </div>
-        </div>
       </header>
-    </div>
+    </>
   );
 }
 
