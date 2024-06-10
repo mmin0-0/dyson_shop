275b48d196b2165ffb400a4f8549e06aa7801e42
Choi minyoung <84059199+mmin0-0@users.noreply.github.com>
2024-06-10 15:22:22 +0900

    ,

--- shop_page/src/components/Cart.jsx
+++ shop_page/src/components/Cart.jsx
@@ -11,104 +11,119 @@ function Cart(){
             <span className="state">0</span>
           </div>
         </div>
-        <div className="table-wrap">
-          <table className="shop-table">
-            <caption>장바구니 테이블</caption>
-            <colgroup>
-              <col width="*"></col>
-              <col width="180px"></col>
-              <col width="180px"></col>
-              <col width="180px"></col>
-            </colgroup>
-            <thead>
-              <tr>
-                <th>
-                  <div className="flex-wrap center">
-                    <div class="input-wrap check">
-                      <input id="allCheck" type="checkbox" name="basket" />
-                      <label for="allCheck"></label>
-                    </div>
-                    <span className="txt">상품정보</span>
-                  </div>
-                </th>
-                <th><span className="txt d-pc">수량</span></th>
-                <th><span className="txt d-pc">주문금액</span></th>
-                <th><span className="txt d-pc">배송정보</span></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td className="img">
-                  <div className="flex-wrap pd-wrap">
-                    <div class="input-wrap check">
-                      <input id="check_01" type="checkbox" name="basket" />
-                      <label for="check_01"></label>
-                    </div>
-                    <div className="pd-info">
-                      <a href="#none">
-                        <div className="img-wrap">상품</div>
-                        <p>[감탄] 어여쁜 어성초 비누 감탄-천연 수제 CP비누</p>
-                      </a>
-                    </div>
-                  </div>
-                  <a href="#none" className="btn-remove">제거</a>
-                </td>
-                <td className="hidden-sm">
-                  <div className="info-list">
-                    <div className="flex-wrap price">
-                      <div className="tit txt-bold">주문금액</div>
-                      <div className="cont txt-bold">6,000원</div>
-                    </div>
-                    <div className="result-list">
-                      <div className="flex-wrap">
-                        <div className="tit">상품금액(총<span>1</span>개)</div>
-                        <div className="cont"><span>6,000</span>원</div>
-                      </div>
-                      <div className="flex-wrap">
-                        <div className="tit">배송비</div>
-                        <div className="cont"><span>3,000</span>원</div>
-                      </div>
-                      <div className="flex-wrap">
-                        <div className="tit">배송수단</div>
-                        <div className="cont">택배</div>
-                      </div>
-                    </div>
-                  </div>
-                </td>
-                <td className="hidden-lg">
-                  <div className="amount">
-                    <div className="tit txt-bold">1</div>
-                    <div className="btn-wrap">
-                      <button type="cart-option">옵션/수량 변경</button>
-                    </div>
-                  </div>
-                </td>
-                <td className="hidden-lg">
-                  <div className="price">
-                    <p className="tit"><span className="txt-bold">6,000</span> 원</p>
-                    <div className="btn-wrap">
-                      <button type="button" className="order-option">바로구매</button>
-                    </div>
-                  </div>
-                </td>
-                <td className="hidden-sm">
-                  <div className="btn-wrap">
-                    <button type="button" className="cart-option">옵션/수량 변경</button>
-                    <button type="button" className="order-option">바로구매</button>
-                  </div>
-                </td>
-                <td className="hidden-lg">
-                  <p className="txt-bold">3,000 원</p>
-                  <span>택배</span>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+        <div className="cont-wrap">
+          <CartTable />
+          <ResultTable />
         </div>
-        <section></section>
       </div>
     </div>
   )
 }
 
+function CartTable(){
+  return (
+    <div className="table-wrap">
+      <table className="shop-table">
+        <caption>장바구니 테이블</caption>
+        <colgroup>
+          <col width="*"></col>
+          <col width="180px"></col>
+          <col width="180px"></col>
+          <col width="180px"></col>
+        </colgroup>
+        <thead>
+          <tr>
+            <th>
+              <div className="flex-wrap center">
+                <div className="input-wrap check">
+                  <input id="allCheck" type="checkbox" name="basket" />
+                  <label htmlFor="allCheck"></label>
+                </div>
+                <span className="txt">전체선택</span>
+              </div>
+            </th>
+            <th><span className="txt d-pc">수량</span></th>
+            <th><span className="txt d-pc">주문금액</span></th>
+            <th><span className="txt d-pc">배송정보</span></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="img">
+              <div className="flex-wrap pd-wrap">
+                <div className="input-wrap check">
+                  <input id="check_01" type="checkbox" name="basket" />
+                  <label htmlFor="check_01"></label>
+                </div>
+                <div className="pd-info">
+                  <a href="#none">
+                    <div className="img-wrap">상품</div>
+                    <p>[감탄] 어여쁜 어성초 비누 감탄-천연 수제 CP비</p>
+                  </a>
+                </div>
+              </div>
+              <a href="#none" className="btn-remove">제거</a>
+            </td>
+            <td className="hidden-sm">
+              <div className="info-list">
+                <div className="flex-wrap price">
+                  <div className="tit txt-bold">주문금액</div>
+                  <div className="cont txt-bold">6,000원</div>
+                </div>
+                <div className="result-list">
+                  <div className="flex-wrap">
+                    <div className="tit">상품금액(총<span>1</span>)</ div>
+                    <div className="cont"><span>6,000</span>원</div>
+                  </div>
+                  <div className="flex-wrap">
+                    <div className="tit">배송비</div>
+                    <div className="cont"><span>3,000</span>원</div>
+                  </div>
+                  <div className="flex-wrap">
+                    <div className="tit">배송수단</div>
+                    <div className="cont">택배</div>
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td className="hidden-lg">
+              <div className="amount">
+                <div className="tit txt-bold">1</div>
+                <div className="btn-wrap">
+                  <button type="cart-option">옵션/수량 변경</button>
+                </div>
+              </div>
+            </td>
+            <td className="hidden-lg">
+              <div className="price">
+                <p className="tit"><span className="txt-bold">000</ span> 원</p>
+                <div className="btn-wrap">
+                  <button type="button"className="order-option">바로구매</button>
+                </div>
+              </div>
+            </td>
+            <td className="hidden-sm">
+              <div className="btn-wrap">
+                <button type="button" className="cart-option">옵수량 변경</button>
+                <button type="button" className="order-option">바로구매</button>
+              </div>
+            </td>
+            <td className="hidden-lg">
+              <p className="txt-bold">3,000 원</p>
+              <span>택배</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+function ResultTable(){
+  return (
+    <div className="table-wrap">
+      하이
+    </div>
+  )
+}
 export default Cart;
