f890688a1569502151041307dbb6d632ae04c136
Choi minyoung <84059199+mmin0-0@users.noreply.github.com>
2024-06-10 14:30:21 +0900

    ,

--- shop_page/src/components/Cart.jsx
+++ shop_page/src/components/Cart.jsx
@@ -12,7 +12,7 @@ function Cart(){
           </div>
         </div>
         <div className="table-wrap">
-          <table>
+          <table className="shop-table">
             <caption>장바구니 테이블</caption>
             <colgroup>
               <col width="*"></col>
@@ -31,14 +31,14 @@ function Cart(){
                     <span className="txt">상품정보</span>
                   </div>
                 </th>
-                <th><span className="txt hidden-sm">수량</span></th>
-                <th><span className="txt hidden-sm">주문금액</span></th>
-                <th><span className="txt hidden-sm">배송정보</span></th>
+                <th><span className="txt d-pc">수량</span></th>
+                <th><span className="txt d-pc">주문금액</span></th>
+                <th><span className="txt d-pc">배송정보</span></th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td>
+                <td className="img">
                   <div className="flex-wrap pd-wrap">
                     <div class="input-wrap check">
                       <input id="check_01" type="checkbox" name="basket" />
@@ -53,10 +53,10 @@ function Cart(){
                   </div>
                   <button type="button" className="btn-remove">제거</button>
                 </td>
-                <td className="hidden-lg">
+                <td className="hidden-sm">
                   <div className="info-list">
-                    <div className="price-result">
-                      <div className="tit">주문금액</div>
+                    <div className="flex-wrap price">
+                      <div className="tit txt-bold">주문금액</div>
                       <div className="cont txt-bold">6,000원</div>
                     </div>
                     <div className="result-list">
@@ -75,19 +75,19 @@ function Cart(){
                     </div>
                   </div>
                 </td>
-                <td className="hidden-sm">
+                <td className="hidden-lg">
                   <div className="count-wrap">
                     <button type="button" className="increase">+</button>
                     <p className="count-num">0</p>
                     <button type="button" className="decrease">-</button>
                   </div>
                 </td>
-                <td>
+                <td className="hidden-lg">
                   <div className="price">
                     <p><span className="txt-bold">6,000</span> 원</p>
                   </div>
                 </td>
-                <td>
+                <td className="hidden-lg">
                   <p className="txt-bold">3,000 원</p>
                   <span>택배</span>
                 </td>
