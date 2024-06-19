c52259105323fd872fdfbe14c146796799f35f67
Choi minyoung <84059199+mmin0-0@users.noreply.github.com>
2024-06-19 17:03:13 +0900

    ,

--- shop_page/src/data.js
+++ shop_page/src/data.js
@@ -3,22 +3,26 @@ const pdList = [
     id : 0,
     title : "White and Black",
     content : "Born in France",
-    price : 120000
+    price : 120000,
+    img: []
   },{
     id : 1,
     title : "Red Knit",
     content : "Born in Seoul",
-    price : 110000
+    price : 110000,
+    img: []
   },{
     id : 2,
     title : "Grey Yordan",
     content : "Born in the States",
-    price : 130000
+    price : 130000,
+    img: []
   },{
     id : 3,
     title : "Grey dank",
     content : "Born in the States",
-    price : 200000
+    price : 200000,
+    img: []
   }
 ];
 
--- shop_page/src/pages/Detail.jsx
+++ shop_page/src/pages/Detail.jsx
@@ -24,6 +24,7 @@ function Detail(props){
               })
             }
           </div>
+          <section></section>
         </div>
       </div>
     </div>
