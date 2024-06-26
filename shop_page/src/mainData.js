491ac7eb27ab19816bb6df65a9eb025ec2ab9704
Choi minyoung <84059199+mmin0-0@users.noreply.github.com>
2024-06-26 12:44:58 +0900

    ,

--- shop_page/src/mainData.js
+++ shop_page/src/mainData.js
@@ -6,12 +6,17 @@ const vacuumInfo = [
     note: 'Suction tested to En 60312-1 Cl 5.8 and 5.9 at the hose/wand, loaded to bin full, tested in Booset mode.',
   },{
     class: 'even',
-    title : ['Radial Root', 'Cyclone', 'technology'],
+    title : ['Heat', 'Pipe', 'technology'],
     content : 'This Dyson Hyperdymium motor spins at up to 125,000rpm to generate more suction power than most corded vacuums.',
     note: 'Suction tested to En 60312-1 Cl 5.8 and 5.9 at the hose/wand, loaded to bin full, tested in Booset mode.',
   },{
     class: null,
-    title : ['High power.', 'High energy', 'High safety.'],
+    title : ['Intelligent', 'heat', 'control'],
+    content : 'This Dyson Hyperdymium motor spins at up to 125,000rpm to generate more suction power than most corded vacuums.',
+    note: 'Suction tested to En 60312-1 Cl 5.8 and 5.9 at the hose/wand, loaded to bin full, tested in Booset mode.',
+  },{
+    class: null,
+    title : ['Intelligent', 'heat', 'conti'],
     content : 'This Dyson Hyperdymium motor spins at up to 125,000rpm to generate more suction power than most corded vacuums.',
     note: 'Suction tested to En 60312-1 Cl 5.8 and 5.9 at the hose/wand, loaded to bin full, tested in Booset mode.',
   }
