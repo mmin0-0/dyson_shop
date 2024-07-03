9ac12c9def28a5435a4fd33f904c2c2692ba8dac
Choi minyoung <84059199+mmin0-0@users.noreply.github.com>
2024-07-03 15:10:54 +0900

    Update data.js

--- shop_page/src/data.js
+++ shop_page/src/data.js
@@ -1,72 +1,94 @@
 const pdList = [
   {
     id : 'hairCare',
-    title : "헤어케어",
-    content : "과도한 열 손상으로부터 모발을 보호 적은 열로 효과적인 스타일링",
+    title : '헤어케어',
+    content : '과도한 열 손상으로부터 모발을 보호 적은 열로 효과적인 스타일링',
     mainImg: 'https://dyson-h.assetsadobe2.com/is/image/content/dam/dyson/home-page/homepage-category-tiles-hair-care-2.jpg',
     data: [
       {
         id: 0,
-        title: '헤어케어01',
-        content: '헤어케어01',
-        price : 120000,
+        title: '다이슨 에어스트레이트 스트레이트너(블루/코퍼)',
+        content: '젖은 모발에 바람으로 드라이와 스트레이트를 동시에 과도한 열 손상 없이',
+        price : 599000,
+        pdImg: 'https://dyson-h.assetsadobe2.com/is/image/content/dam/dyson/leap-petite-global/dynamic-media/personal-care/553/primary/553-Primary-DKBLBCO_800x1200.png?scl=1&fmt=png-alpha'
       },{
         id: 1,
-        title: '헤어케어01',
-        content: '헤어케어02',
-        price : 120000,
+        title: '다이슨 슈퍼소닉 뉴럴 헤어드라이어(빈카블루/토파즈)',
+        content: '뉴럴 센서 네트워크가 자동으로 바람 속도와 온도를 조절하여 모발 본연의 자연스러운 윤기를 유지하고 두피 건강을 보호하는 데 도움을 줍니다.',
+        price : 599000,
+        pdImg: 'https://dyson-h.assetsadobe2.com/is/image/content/dam/dyson/leap-petite-global/dynamic-media/personal-care/605g/primary/605GM-Primary-VIBLTRTPOR-Tampo.png?scl=1&fmt=png-alpha'
       }
     ]
   },{
     id : 'cleaner',
-    title : "청소기",
-    content : "진공청소에서 물청소, 로봇 청소기까지 일상 속 다양한 청소 문제를 다이슨으로 해결하세요.",
+    title : '청소기',
+    content :'진공청소에서 물청소, 로봇 청소기까지 일상 속 다양한 청소 문제를 다이슨으로 해결하세요.',
     mainImg: 'https://dyson-h.assetsadobe2.com/is/image/content/dam/dyson/home-page/homepage-category-tiles-floorcare.jpg',
     data: [
       {
         id: 0,
-        title: '청소기01',
-        content: '청소기01',
-        price : 110000,
+        title: '다이슨 V15 디텍트 (옐로/니켈)',
+        content: '일루미네이션 기술이 적용되어 미세한 크기의 먼지를 보여줍니다.',
+        price : 1090000,
+        pdImg: 'https://dyson-h.assetsadobe2.com/is/image/content/dam/dyson/leap-petite-global/products/floorcare/sticks/v15-detect/pdp/dynamic-media/primary/SV22-CORE-LB.png?fmt=png-alpha&scl=1&fmt=png-alpha'
+      },{
+        id: 1,
+        title: '다이슨 디지털 슬림',
+        content: '청소 성능은 그대로 어디서나 강력한 청소 / 1.9kg 가벼운 무게 / 지능적인 실시간 LCD 디스플레이 / 3가지 툴 제공',
+        price : 599000,
+        pdImg: 'https://dyson-h.assetsadobe2.com/is/image/content/dam/dyson/leap-petite-global/products/floorcare/sticks/dyson-digital-slim/variants/nickel/hero.png?fmt=png-alpha&scl=1&fmt=png-alpha'
       }
     ]
   },{
     id : 'airPurifier',
-    title : "공기청정기",
-    content : "가습 공기청정기, 공기청정기, 공기청정 온풍기 및 선풍기로 주변 환경을 쾌적하게 관리하세요.",
+    title : '공기청정기',
+    content : '가습 공기청정기, 공기청정기, 공기청정 온풍기 및 선풍기로 주변 환경을 쾌적하게 관리하세요.',
     mainImg: 'https://dyson-h.assetsadobe2.com/is/image/content/dam/dyson/home-page/homepage-category-tiles-air-treatment-2.jpg',
     data: [
       {
         id: 0,
-        title: '공기청정기01',
-        content: '공기청정기01',
-        price : 130000,
+        title: '다이슨 쿨 공기청정기 (화이트/실버)',
+        content: '공기 정화와 냉풍 기능을 하나로 0.1마이크론 크기의 작은 미세먼지를 99.95% 제거합니다.',
+        price : 599000,
+      },{
+        id: 1,
+        title: '다이슨 쿨 선풍기 타워형 (화이트/ 실버)',
+        content: '다이슨 쿨 선풍기 타워형 날개 없는 선풍기의 강력한 바람',
+        price : 398000,
       }
     ]
   },{
     id : 'headphones',
-    title : "헤드폰",
-    content : "몰입감 넘치는 편안한 청취. 액티브 노이즈 캔슬링으로 최대 50시간 오디오 재생",
+    title : '헤드폰',
+    content : '몰입감 넘치는 편안한 청취. 액티브 노이즈 캔슬링으로 최대 50시간 오디오 재생',
     mainImg: 'https://dyson-h.assetsadobe2.com/is/image/content/dam/dyson/home-page/homepage-category-tiles-headphones.jpg',
     data: [
       {
         id: 0,
-        title: '헤드폰01',
-        content: '헤드폰01',
-        price : 200000,
+        title: '다이슨 존 앱솔루트 플러스 노이즈 캔슬링 헤드폰 (블루/코퍼)',
+        content: '깨끗한 다이슨 오디오 음질과 우수한 노이즈 캔슬링. 사운드 왜곡을 최소화 하기위한 설계.',
+        price : 990000,
+        pdImg: 'https://dyson-h.assetsadobe2.com/is/image/content/dam/dyson/leap-petite-global/hero/wearables/Hero-Zone-Blue-Copper.png'
       }
     ]
   },{
     id : 'lighting',
-    title : "조명",
-    content : "자연광 시간을 추적해 최적의 빛을 제공합니다.",
+    title : '조명',
+    content : '자연광 시간을 추적해 최적의 빛을 제공합니다.',
     mainImg: 'https://dyson-h.assetsadobe2.com/is/image/content/dam/dyson/home-page/homepage-category-tiles-lighting.jpg',
     data: [
       {
         id: 0,
-        title: '조명01',
-        content: '조명01',
-        price : 200000,
+        title: '다이슨 솔라사이클 모프 조명 플로어스탠드형(화이트/실버)',
+        content: '다이슨 솔라사이클 모프로 나에게 딱 맞는 분위기를 연출해 보세요.',
+        price : 960000,
+        pdImg: 'https://dyson-h.assetsadobe2.com/is/image/content/dam/dyson/leap-petite-global/dynamic-media/lighting/lightcycle-morph/primary/white-silver-floor-primary.png?fmt=png-alpha&scl=1&fmt=png-alpha'
+      },{
+        id: 1,
+        title: '다이슨 솔라사이클 모프 조명 데스크형(화이트/실버)',
+        content: '지능적으로 사용자 위치의 자연광 추적 / 사용자와 주변 환경에 따라 맞춤 변환, 조절 / 4가지 조명을 하나로',
+        price : 720000,
+        pdImg: 'https://dyson-h.assetsadobe2.com/is/image/content/dam/dyson/leap-petite-global/dynamic-media/lighting/lightcycle-morph/primary/white-silver-desk-primary.png?fmt=png-alpha&scl=1&fmt=png-alpha'
       }
     ]
   }
