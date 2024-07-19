b740d6bf52d901f16c579c48623ca986bf3a519c
Choi minyoung <84059199+mmin0-0@users.noreply.github.com>
2024-07-19 16:32:12 +0900

    ,

--- shop_page/src/App.js
+++ shop_page/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import QuickMenu from './components/QuickMenu.jsx';
 import Error from './pages/404_error.jsx';
+import Search from './pages/Search.jsx';
 import Home from './pages/Home.jsx';
 import Cart from './pages/Cart.jsx';
 import Detail from './pages/Detail.jsx';
@@ -23,6 +24,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home product={product} />} />
         <Route path="*" element={<Error />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/cart" element={<Cart />} price={price} />
         <Route path="/detail" element={<Detail product={product} price={price} />} />
         <Route path="/detail/:id/:dataId" element={<PdDetail product={product} price={price} />} />
---
+++ shop_page/src/pages/Search.jsx
@@ -0,0 +1,16 @@
+function Search(){
+  return(
+    <div id="wrap" className="search">
+      <div className="wrap-inner">
+        <h2>검새결과</h2>
+        <ul>
+          <li>
+
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Search;
