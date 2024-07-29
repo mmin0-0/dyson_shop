559dd8d80ed83124b9a62aab5584072a4e7e3fb1
Choi minyoung <84059199+mmin0-0@users.noreply.github.com>
2024-07-29 13:50:02 +0900

    Update Modal.jsx

--- shop_page/src/components/Modal.jsx
+++ shop_page/src/components/Modal.jsx
@@ -49,78 +49,74 @@ function Modal(){
 }
 
 function Member({togglePwVisible}){
+  const snsLogin = [
+    {
+      id: 'naver',
+      tit: '네이버'
+    },{
+      id: 'kakao',
+      tit: '카카오'
+    },{
+      id: 'google',
+      tit: '구굴'
+    },{
+      id: 'apple',
+      tit: '구글'
+    }
+  ];
+
   return(
     <div>
-                <div className="login-info">
-                  <form>
-                    <div className="input-wrap">
-                      <input type="text" id="userId" placeholder="아이디를 입력하세요" />
-                      <label htmlFor="userId" className="hide">회원 아이디</label>
-                    </div>
-                    <div className="input-wrap">
-                      <input type="password" id="userPw" placeholder="비밀번호를 입력하세요" />
-                      <span className="pw-icon" onClick={togglePwVisible}></span>
-                      <label htmlFor="userPw" className="hide">회원 비밀번호</label>
-                    </div>
-                  </form>
-                </div>
-                <div className="check-form">
-                  <div className="input-wrap check">
-                    <input type="checkbox" id="keepLogin" />
-                    <label htmlFor="keepLogin">로그인 상태 유지</label>
+      <div className="login-info">
+        <form>
+          <div className="input-wrap">
+            <input type="text" id="userId" placeholder="아이디를 입력하세요" />
+            <label htmlFor="userId" className="hide">회원 아이디</label>
+          </div>
+          <div className="input-wrap">
+            <input type="password" id="userPw" placeholder="비밀번호를 입력하세요" />
+            <span className="pw-icon" onClick={togglePwVisible}></span>
+            <label htmlFor="userPw" className="hide">회원 비밀번호</label>
+          </div>
+        </form>
+      </div>
+      <div className="check-form">
+        <div className="input-wrap check">
+          <input type="checkbox" id="keepLogin" />
+          <label htmlFor="keepLogin">로그인 상태 유지</label>
+        </div>
+        <div className="input-wrap check">
+          <input type="checkbox" id="keepUserId" />
+          <label htmlFor="keepUserId">아이디 저장</label>
+        </div>
+      </div>
+      <div className="login-btn">
+        <button type="button">로그인</button>
+      </div>
+      <div className="login-txt">
+        <a href="javascript:void(0)">아이디 찾기</a>
+        <a href="javascript:void(0)">비밀번호 찾기</a>
+      </div>
+      <div className="sns-login">
+        <div className="sns-login-tit">
+          <span>또는 간편하게 로그인</span>
+        </div>
+        <ul className="sns-login-list">
+          {
+            snsLogin.map((a,i)=>(
+              <li key={i}>
+                <a href="javascript:void(0)" id={`${a.id}Ligin`}>
+                  <div className="icon">
+                    <img src={`${process.env.PUBLIC_URL}/images/icon/login_${a.id}_icon.png`} alt={`${a.id} login`} />
                   </div>
-                  <div className="input-wrap check">
-                    <input type="checkbox" id="keepUserId" />
-                    <label htmlFor="keepUserId">아이디 저장</label>
-                  </div>
-                </div>
-                <div className="login-btn">
-                  <button type="button">로그인</button>
-                </div>
-                <div className="login-txt">
-                  <a href="javascript:void(0)">아이디 찾기</a>
-                  <a href="javascript:void(0)">비밀번호 찾기</a>
-                </div>
-                <div className="sns-login">
-                  <div className="sns-login-tit">
-                    <span>또는 간편하게 로그인</span>
-                  </div>
-                  <ul className="sns-login-list">
-                    <li>
-                      <a href="javascript:void(0)">
-                        <div className="icon">
-                          <img src={`${process.env.PUBLIC_URL}/images/icon/login_naver_icon.png`} alt="naver login" />
-                        </div>
-                        <span>네이버</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="javascript:void(0)">
-                        <div className="icon">
-                          <img src={`${process.env.PUBLIC_URL}/images/icon/login_kakao_icon.png`} alt="kakao login" />
-                        </div>
-                        <span>카카오톡</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="javascript:void(0)">
-                        <div className="icon">
-                          <img src={`${process.env.PUBLIC_URL}/images/icon/login_google_icon.png`} alt="google login" />
-                        </div>
-                        <span>구글</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="javascript:void(0)">
-                        <div className="icon">
-                          <img src={`${process.env.PUBLIC_URL}/images/icon/login_apple_icon.png`} alt="apple login" />
-                        </div>
-                        <span>애플</span>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+                  <span>{a.tit}</span>
+                </a>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    </div>
   )
 }
 
